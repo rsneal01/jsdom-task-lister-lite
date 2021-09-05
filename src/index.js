6f44a8e0e8052f9ccfa368ca45ba6df5cb036c8e
rsneal01 <rsneal01@gmail.com>
2021-09-05 10:08:59 -0700

    stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const newForm  = document.getElementById('create-task-form');
+  const newDescription = document.getElementById('new-task-description');
+
+  newForm.addEventListener('submit', createNewTask);
+    // handle the form data
+  });
+
+  const createNewTask = event => {
+    event.preventDefault();
+    //stop form from trying to submit
+    const newDescription = document.getElementById("new-task-description");
+    const newTask = document.createElement("li");
+    newTask.innerText = newDescription.value;
+
+    appendNewTask(newTask);
+    // event.target.reset();
+};
+
+
+function appendNewTask(task) {
+  document.getElementById("tasks").appendChild(task);
+}
